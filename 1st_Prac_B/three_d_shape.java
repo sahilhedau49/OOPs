387f99c4e68236f7098f98c0bdaa95f2d957489b
sahilhedau49 <hedausahil49@gmail.com>
2023-08-06 15:25:14 +0530

    Update three_d_shape.java

--- 1st_Prac_B/three_d_shape.java
+++ 1st_Prac_B/three_d_shape.java
@@ -1,16 +1,19 @@
 // package 1st_Prac_B;
 
 public class three_d_shape {
-    double volume(double radius){
-        return 4*Math.PI*radius*radius*radius/3;
+    double volume(double radius) {
+        return 4 * Math.PI * radius * radius * radius / 3;
     }
-    double volume(int l, int b, int h){
-        return l*b*h; 
+
+    double volume(int l, int b, int h) {
+        return l * b * h;
     }
-    double volume(int r, int h){
-        return Math.PI*r*r*h;
+
+    double volume(int r, int h) {
+        return Math.PI * r * r * h;
     }
-    double volume(int side){
-        return side*side*side;
+
+    double volume(int side) {
+        return side * side * side;
     }
 }
